ee990256df8104e97e3918c733f7463e6b66140b
hannah-marcus28 <103459472+hannah-marcus28@users.noreply.github.com>
2023-06-12 21:58:33 +0000

    Adding Jinja to product_type_sales

--- coffee_shop_hannah/models/product_type_sales.sql
+++ coffee_shop_hannah/models/product_type_sales.sql
@@ -1,8 +1,14 @@
+{% set product_categories = ['coffee beans', 'merch', 'brewing supplies']}
+
 select
   date_trunc(start_of_week, month) as date_month,
-  sum(case when category = 'coffee beans' then revenue end) as coffee_beans_amount,
-  sum(case when category = 'merch' then revenue end) as merch_amount,
-  sum(case when category = 'brewing supplies' then revenue end) as brewing_supplies_amount
+  {% for product_category in product_categories %}
+  sum(case when category = {{product_category}} then revenue end) as {{product_category}}_amount
+  {% if not loop.last %},{% endif %}
+  {% endfor %}
+  --sum(case when category = 'coffee beans' then revenue end) as coffee_beans_amount,
+  --sum(case when category = 'merch' then revenue end) as merch_amount,
+  --sum(case when category = 'brewing supplies' then revenue end) as brewing_supplies_amount
 -- you may have to `ref` a different model here, depending on what you've built previously
 from {{ ref('weekly_sales') }}
 group by 1
